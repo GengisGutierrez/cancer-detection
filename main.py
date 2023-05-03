038ee5a0b7225ffe36bee83576b570768bc74d04
gengis gutierrez <gengisgutierrezc@pacifico.com.pe>
2023-05-03 07:49:57 -0500

    Finish pix2pix gan version 0.1, Next step: Improve dataset and use other architecture pix2pixhd

--- main.py
+++ main.py
@@ -1,109 +1,105 @@
-import tensorflow as tf
-
 import os
 import time
 import datetime
-
+import tensorflow as tf
 from IPython import display
+
 from matplotlib import pyplot as plt
 from data_ingest import input_pipeline
+from components_gan import Generator, Discriminator, generator_loss, discriminator_loss
+
+generator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+discriminator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+generator = Generator()
+discriminator = Discriminator()
+checkpoint_dir = "./training_checkpoints"
+checkpoint_prefix = os.path.join(checkpoint_dir, 'ckpt')
+checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                discriminator_optimizer=discriminator_optimizer,
+                                generator=generator,
+                                discriminator=discriminator)
+log_dir = "logs/"
+
+summary_writer = tf.summary.create_file_writer(
+    log_dir + "fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
 
 
-OUTPUT_CHANNELS = 3
-LAMBDA = 100
-loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+def generate_images(model, test_input, tar):
+    prediction = model(test_input, training=True)
+    plt.figure(figsize=(15, 15))
+    display_list = [test_input[0], tar[0], prediction[0]]
+    title = ['Input Image', 'Labeled Image', 'Predicted Image']
+
+    for i in range(3):
+        plt.subplot(1, 3, i+1)
+        plt.title(title[i])
+        plt.imshow(display_list[i] * 0.5 + 0.5)
+        plt.axis('off')
+    plt.show()
 
 
-def downsample(filters, size, apply_batchnorm=True):
-    initializer = tf.random_normal_initializer(0., 0.02)
-    result = tf.keras.Sequential()
-    result.add(
-        tf.keras.layers.Conv2D(filters, size, strides=2, padding='same',
-                               kernel_initializer=initializer, use_bias=False))
-    if apply_batchnorm:
-        result.add(tf.keras.layers.BatchNormalization())
-    result.add(tf.keras.layers.LeakyReLU())
-    return result
+@tf.function
+def train_step(input_image, target, step):
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        gen_output = generator(input_image, training=True)
+
+        disc_real_output = discriminator([input_image, target], training=True)
+        disc_generated_output = discriminator([input_image, gen_output], training=True)
+
+        gen_total_loss, gen_gan_loss, gen_l1_loss = generator_loss(disc_generated_output, gen_output, target)
+        disc_loss = discriminator_loss(disc_real_output, disc_generated_output)
+
+    generator_gradients = gen_tape.gradient(gen_total_loss,
+                                            generator.trainable_variables)
+    discriminator_gradients = disc_tape.gradient(disc_loss,
+                                                 discriminator.trainable_variables)
+    generator_optimizer.apply_gradients(zip(generator_gradients,
+                                            generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(discriminator_gradients,
+                                                discriminator.trainable_variables))
+    with summary_writer.as_default():
+        tf.summary.scalar('gen_total_loss', gen_total_loss, step=step//1000)
+        tf.summary.scalar('gen_gan_loss', gen_gan_loss, step=step//1000)
+        tf.summary.scalar('gen_l1_loss', gen_l1_loss, step=step//1000)
+        tf.summary.scalar('disc_loss', disc_loss, step=step//1000)
 
 
-def upsample(filters, size, apply_dropout=False):
-    initializer = tf.random_normal_initializer(0., 0.02)
-    result = tf.keras.Sequential()
-    result.add(
-        tf.keras.layers.Conv2DTranspose(filters, size, strides=2,
-                                        padding='same',
-                                        kernel_initializer=initializer,
-                                        use_bias=False))
-    result.add(tf.keras.layers.BatchNormalization())
-    if apply_dropout:
-        result.add(tf.keras.layers.Dropout(0.5))
-    result.add(tf.keras.layers.ReLU())
-    return result
+def fit(train_ds, test_ds, steps):
+    example_input, example_target = next(iter(test_ds.take(1)))
+    start = time.time()
 
+    for step, (input_image, target) in train_ds.repeat().take(steps).enumerate():
+        if (step) % 1000 == 0:
+            display.clear_output(wait=True)
+            if step != 0:
+                print(f'Time taken for 1000 steps: {time.time()-start:.2f} sec\n')
+            start = time.time()
+            generate_images(generator, example_input, example_target)
+            print(f"Step: {step//1000}k")
 
-def Generator():
-    inputs = tf.keras.layers.Input(shape=[256, 256, 3])
+        train_step(input_image, target, step)
 
-    down_stack = [
-        downsample(64, 4, apply_batchnorm=False),
-        downsample(128, 4),
-        downsample(256, 4),
-        downsample(512, 4),
-        downsample(512, 4),
-        downsample(512, 4),
-        downsample(512, 4),
-        downsample(512, 4),
-    ]
+        # Training step
+        if (step + 1) % 10 == 0:
+            print('.', end='', flush=True)
 
-    up_stack = [
-        upsample(512, 4, apply_dropout=True),
-        upsample(512, 4, apply_dropout=True),
-        upsample(512, 4, apply_dropout=True),
-        upsample(512, 4),
-        upsample(256, 4),
-        upsample(128, 4),
-        upsample(64, 4),
-    ]
-
-    initializer = tf.random_normal_initializer(0., 0.02)
-    last = tf.keras.layers.Conv2DTranspose(OUTPUT_CHANNELS, 4,
-                                           strides=2,
-                                           padding='same',
-                                           kernel_initializer=initializer,
-                                           activation='tanh')
-    x = inputs
-
-    # Downsampling throught the model
-    skips = []
-    for down in down_stack:
-        x = down(x)
-        skips.append(x)
-    skips = reversed(skips[:-1])
-
-    # Upsampling and establishing the skip conecctions
-    for up, skip in zip(up_stack, skips):
-        x = up(x)
-        x = tf.keras.layers.Concatenate()([x, skip])
-
-    x = last(x)
-
-    return tf.keras.Model(inputs=inputs, outputs=x)
+        # Save (checkpoint) the model every 5k steps
+        if (step + 1) % 5000 == 0:
+            checkpoint.save(file_prefix=checkpoint_prefix)
 
 
 def main():
-    global img
     train_dataset, val_dataset = input_pipeline()
-    for train_image, train_label in train_dataset:
-        img = train_image[0]
-        lbl = train_label[0].numpy()
+    fit(train_dataset, val_dataset, steps=40000)
 
-    print(train_dataset)
-    generator = Generator()
-    gen_output = generator(img[tf.newaxis, ...], training=False)
-    print(type(gen_output))
-    plt.figure()
-    plt.imshow(gen_output[0]/ tf.math.reduce_max(gen_output[0]))
-    plt.show()
+
+
+
+    # for example_input, example_target in val_dataset.take(1):
+    #     generate_images(generator, example_input, example_target)
+
+
 
 
 if __name__ == "__main__":
